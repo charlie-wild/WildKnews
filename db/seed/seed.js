a1f9b4818dc1d45754ae2bec38f1d44413931f63
charlie-wild <charliewild@gmail.com>
2018-12-06 16:47:27 +0000

    testing and ready to seed

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -1,14 +1,16 @@
-const topicsData = require('../data/test-data/topics');
-const userData = require('../data/test-data/users');
-const articleData = require('../data/test-data/articles');
-const commentData = require('../data/test-data/comments');
+const {
+  articleData,
+  topicData,
+  commentData,
+  userData
+} = require('../data/index.js');
 const {
   userIdSetter, formatArticles, articleIdSetter,
   formatComments,
 } = require('../utils');
 
 exports.seed = function (knex, Promise) {
-  return Promise.all([knex('topics').del(), knex('users').del(), knex('articles').del(), knex('comments').del()]).then(() => knex('topics').insert(topicsData).returning('*'))
+  return Promise.all([knex('topics').del(), knex('users').del(), knex('articles').del(), knex('comments').del()]).then(() => knex('topics').insert(topicData).returning('*'))
     .then(() => knex('users').insert(userData).returning('*'))
     .then((userRows) => {
       const userlookup = userIdSetter(userRows);
