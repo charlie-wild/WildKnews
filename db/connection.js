a5280922e81c7db8d568193c097ee1c0e1e95be2
charlie-wild <charliewild@gmail.com>
2018-12-07 14:30:47 +0000

    set up for deployment with heroku

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const ENV = process.env.NODE_ENV || 'development';
 const knex = require('knex');
 
-const dbConfig = require('../knexfile.js')[ENV];
+const dbConfig = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile.js')[ENV];
 
 const connection = knex(dbConfig);
 
