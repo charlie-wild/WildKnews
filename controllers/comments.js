848dbca7575e35711b96b1f5afb8b7cdaea57a73
charlie-wild <charliewild@gmail.com>
2019-01-08 17:23:18 +0000

    removing non functioning comment sort criteria

--- controllers/comments.js
+++ controllers/comments.js
@@ -5,7 +5,7 @@ exports.getAllComments = (req, res, next) => {
     limit = 10, sort_by = 'created_at', p = 1, sort_ascending,
   } = req.query;
   return connection('comments')
-    .select('comments.comment_id', 'comments.votes', 'comments.created_at', 'users.username AS author', 'comments.body', 'comments.belongs_to')
+    .select('comments.comment_id', 'comments.votes', 'comments.created_at', 'users.username AS author', 'comments.body')
     .offset(Math.floor(limit * (p - 1)))
     .modify((articleQuery) => {
       if (sort_ascending) {
